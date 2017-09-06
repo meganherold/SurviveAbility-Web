628d60b8f9a3402180f8084be095219b20aa2ce5
Megan McPherson <meganmcp95@gmail.com>
2017-09-05 23:52:36 -0700

    added a json file to store captains and api paths for getting and posting captains.

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 var express = require('express');
 var app = express();
+var fs = require("fs");
+var bodyParser = require('body-parser')
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json())
 
-// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/api/captains', function (request, response) {
+   fs.readFile( __dirname + "/data/captains.json", 'utf8', function (error, data) {
+       response.end(data);
+   });
+});
+
+app.post('/api/addCaptain', function (request, response) {
+  var database;
+  fs.readFile( __dirname + "/data/captains.json", 'utf8', function (err, data) {
+      console.log("data: " + JSON.parse(data))
+      database = JSON.parse(data);
+      database.captains.push(request.body)
+
+      fs.writeFile(__dirname + "/data/captains.json", JSON.stringify(database), function(error) {
+        if(error) {
+            response.end("There was a problem writing the data.")
+        }
+        response.end(JSON.stringify(database));
+      });
+  });
 });
 
 app.listen(app.get('port'), function() {
